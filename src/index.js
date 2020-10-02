c7d2a4fe84dfc8c5526a25351af9683e9ac81b1a
anastasia1311 <nastiadardinskaya@tut.by>
2020-10-02 19:09:13 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+
+    if(n > 0){
+        return (parseFloat(n.toString().split('').reverse().join('')) * Math.sign(n));    
+   }
+   else{
+        return Math.abs((parseFloat(n.toString().split('').reverse().join('')) * Math.sign(n)));  
+   } 
+         
 }
